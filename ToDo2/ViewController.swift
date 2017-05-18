80dc8263eb86059ecce1dd203043a8ed5d247ef3
04minolimi <k10ktok10@gmail.com>
2017-05-18 17:47:18 +0900

    Initial Commit

---
+++ ToDo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo2
+//
+//  Created by fukumoriminori on 2017/05/18.
+//  Copyright © 2017年 fukumoriminori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
