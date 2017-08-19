76fea63b4ae538db3008343cdbd9b80293df8cff
04minolimi <k10ktok10@gmail.com>
2017-08-20 02:12:39 +0900

    SIGABRT倒した〜〜〜〜寝るぞ〜〜〜〜tabbar消えたけど

---
+++ ToDo2/WAddToDoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WAddToDoViewController.swift
+//  ToDo2
+//
+//  Created by fukumoriminori on 2017/08/20.
+//  Copyright © 2017年 fukumoriminori. All rights reserved.
+//
+
+import UIKit
+
+class WAddToDoViewController: UIViewController {
+
+    @IBOutlet weak var WTextField: UITextField!
+    @IBOutlet weak var WAddButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
